9f50ee25f8b0a858c2c8a93dd4ea7480496e4ae6
Souad Allahham <souadallahham89@gmail.com>
2021-03-01 16:26:11 +0100

    I finished Ex-3

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,23 @@
-// write your code below
+// FOR loop
+for(let i = 0; i <= 10; i++){
+   console.log(i)
+  }
+// make an array of the numbers from the previous loop
+let array = [];
+  for (i = 0; i <= 10; i++) {
+      array.push(i);
+  }
+console.log(array);
+
+// multiplication function
+function myFunction(array,index) {
+    for(let i = 0; i < array.length; i++){
+        console.log(array[i]*index)
+       }  
+}
+
+//some examples of time tables
+myFunction(array,9);
+myFunction(array,5);
+myFunction(array,2);
+
