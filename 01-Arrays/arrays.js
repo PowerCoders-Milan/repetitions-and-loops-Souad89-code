87286882aa3d164e4e6bc43c2857e7257de8e0a2
Souad Allahham <souadallahham89@gmail.com>
2021-03-01 11:46:26 +0100

    I finished ex-1

--- 01-Arrays/arrays.js
+++ 01-Arrays/arrays.js
@@ -1,13 +1,20 @@
 // use this array
-var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+let array = ["Banana", "Apples", "Oranges", "Blueberries"];
 
 // Access and output Oranges.
-
+console.log(array[2]);
+// Remove "Banana" from the array
+array.shift();
+console.log(array);
 // Sort the array in alphabetical order.
-
+array.sort();
+console.log(array);
 // Put "Kiwi" at the end of the array.
-
+array.push("Kiwi");
+console.log(array);
 // Remove "Apples" from the array.
-
+array.shift();
+console.log(array);
 // Sort the array in reverse order, i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
-
+array.reverse();
+console.log(array);
