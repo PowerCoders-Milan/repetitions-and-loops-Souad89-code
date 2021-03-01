a8924a31bb1574ef24fb22dcb2f598861ccab7a7
Souad Allahham <souadallahham89@gmail.com>
2021-03-01 14:39:33 +0100

    I finished ex-2

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -1,7 +1,44 @@
 // odd or even reporter
 
+// The solution using FOR
 // write a for loop to iterate from 0 to 20
+/*for(i = 0; i < 21; i++) {
+  console.log(i);
+}
+  // check if the current is even or odd and report the result to the screen.
+  for(i = 0; i < 21; i++) {
+    if (i === 0) {
+      console.log(i +  " is even");
+}
+    else if (i % 2 === 0) {
+      console.log(i + " is even");   
+}
+    else {
+      console.log(i + " is odd");
+}
+  }*/
 
-  // check if the current is even or odd
-
-  // report the result to the screen
+// The solution using WHILE ( here I tried to improve my code)
+/*let num = 0;
+while(num <= 20){
+ 
+  if (num % 2 === 0) {
+    console.log(`the number ${num} is even`);   
+}
+  else {
+    console.log(`the number ${num} is odd`);
+}
+num++;
+}*/
+// The solution using WHILE DO
+let x = 0;
+do {
+  if (x % 2 === 0) {
+    console.log(`the number ${x} is even`);   
+}
+  else {
+    console.log(`the number ${x} is odd`);
+}
+  x++;
+}
+while (x <= 20);
